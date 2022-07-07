96612c2004a3474f07c9e99f60a3bebb35975175
gautham1976 <gautham1976@gmail.com>
2022-07-08 00:27:07 +0530

    Search restaurant completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ViewfoodComponent } from './viewfood/viewfood.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    ViewfoodComponent
+    ViewfoodComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
